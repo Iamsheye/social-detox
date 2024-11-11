d135c7bd63fd7a90cc714e65670b7fcd15e44b5f
Iamsheye <seyepelumi@gmail.com>
2024-11-11 15:50:49 +0100

    removed unused permissions, content scripts & content runtime

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -17,7 +17,7 @@ const manifest = {
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
   host_permissions: ['<all_urls>'],
-  permissions: ['storage', 'scripting', 'tabs', 'idle'],
+  permissions: ['storage', 'tabs', 'idle'],
   options_page: 'options/index.html',
   background: {
     service_worker: 'background.iife.js',
@@ -33,15 +33,7 @@ const manifest = {
   content_scripts: [
     {
       matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content/index.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
       js: ['content-ui/index.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      css: ['content.css'], // public folder
     },
   ],
   web_accessible_resources: [
